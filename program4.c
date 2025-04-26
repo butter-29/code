a0a30911119be9f0cad32715c3b8ecc0204a37e6
butter-29 <mukeshchand10mc@gmail.com>
2025-04-26 11:02:54 +0530

    program4.c

--- program4.c
+++ program4.c
@@ -31,3 +31,22 @@ int main() {
         printf("\nShortest distance from %d to %d is %d", s, i, d[i]);
     return 0;
 }
+
+
+
+ Input/Output
+ Enternvalue:6
+ Enterthegraphdata:
+ 0151099945999
+ 99901599920999
+ 20999020999999
+ 99910999035999
+ 999999999300999
+ 99999999949990
+ Enterthesoucenode:2
+ Shortestdistancefrom2to1is35
+ Shortestdistancefrom2to2is0
+ Shortestdistancefrom2to3is15
+ Shortestdistancefrom2to4is35
+ Shortestdistancefrom2to5is20
+ Shortestdistancefrom2to6is999
